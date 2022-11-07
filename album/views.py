52bea46f19da52a9665cbc35249a477c9207cf88
Anna Belova <aa.belova@sov-teh.com>
2022-11-07 18:00:57 +0900

    delete comments

--- album/views.py
+++ album/views.py
@@ -11,42 +11,11 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 class AlbumApiView(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-
-    # def get_permissions(self):
-    #     if self.request.method == 'GET':
-    #         self.permission_classes = [AllowAny, ]
-    #     else:
-    #         self.permission_classes = [IsAuthenticated, ]
-
-    #     return super(AlbumApiView, self).get_permissions()
-
-
-
-    # def create(self, request, *args, **kwargs):
-    #     """
-    #         Create a list of model instances if a list is provided or a
-    #         single model instance otherwise.
-    #     """
-    #     try:
-    #         data = request.data
-    #         if isinstance(data, list):
-    #             serializer = self.get_serializer(data=data, many=True)
-    #         else:
-    #             serializer = self.get_serializer(data=data)
-    #         serializer.is_valid(raise_exception=True)
-
-    #         serializer.save(user_id=self.request.user.id)
-    #         headers = self.get_success_headers(serializer.data)
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    #     except (TypeError, ValueError) as error:
-    #         return Response({'error': f'{error.__class__.__name__}: {error}'}, status=status.HTTP_400_BAD_REQUEST)
-    
     
 class SongApiView(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
-    
-    
+      
 class SingerApiView(viewsets.ModelViewSet):
     queryset = Singer.objects.all()
     serializer_class = SingerSerializer
@@ -54,3 +23,4 @@ class SingerApiView(viewsets.ModelViewSet):
 class SongAlbumApiView(viewsets.ModelViewSet):
     queryset = SongAlbum.objects.all()
     serializer_class = SongAlbumSerializer
+    
